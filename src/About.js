8a248c9a82f9acca49b9cb4a9ae7722347710c0a
iamdayo <ppldayo@gmail.com>
2022-09-04 06:28:54 +0100

    styled the about page

--- src/About.js
+++ src/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 function About() {
   return (
       <div name="about" className='text-gray-200 '>
-          <div className='p-2 my-2  max-w-[350px] md:max-w-[750px]'>
+          <div className='p-2 my-2  max-w-[320px] md:max-w-[750px]'>
           <div className='ml-6'>
                   <p className='border-b-4 text-2xl border-pink-500 inline'>About</p>
           <p className='py-4'>i'm a front-end developer specialize in building  nice and eye-catchy web app and doing the server side along side computing the data in a decent and clean manner, i'm major in building responsive web app.Generally, i work with software testing specialists to test the codes before the web application or website goes live. I also work with a team of developers, programmers, web designers, and project managers to work closely on a client’s software project.</p>
